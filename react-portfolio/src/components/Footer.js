cd68f60f5305e011317338bc905bfe7cbc6e4dee
Ahmed Alasow <a.alasow01@gmail.com>
2023-02-28 23:03:22 +0000

    Created footer file to link github and linkedin profiles

---
+++ react-portfolio/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Footer(){
+    return(
+        <footer>
+            <p>
+                <a href='https://github.com/Alasow2001'>
+                    <img src='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' alt='My Github Page'/>
+                </a>
+            </p>
+
+            <p>
+                <a href='https://www.linkedin.com/in/ahmed-alasow-80b678215/'>
+                    <img src='https://cdn-icons-png.flaticon.com/512/174/174857.png' alt='My LinkedIn Profile' />
+                </a>
+            </p>
+
+            <p>All rights reserved &copy; Your Name</p>
+        </footer>
+    )
+}
