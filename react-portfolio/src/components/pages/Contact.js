1fc651b16777340510776addb3db982d3954c08f
Ahmed Alasow <a.alasow01@gmail.com>
2023-02-28 22:01:23 +0000

    Made form for users to use

--- react-portfolio/src/components/pages/Contact.js
+++ react-portfolio/src/components/pages/Contact.js
@@ -5,17 +5,26 @@ export default function Contact() {
     <div>
       <h1>Contact Page</h1>
       <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+        <form>
+          <div>
+            <label>Name</label>
+            <input type="text" placeholder='Your Name'/>
+          </div>
+
+          <div>
+            <label>Email</label>
+            <input type='email' placeholder='Your Email'/>
+          </div>
+
+          <div>
+            <label>Message</label>
+            <textarea placeholder='Your Message' />
+          </div>
+
+          <div>
+            <button>Submit Form</button>
+          </div>
+        </form>
       </p>
     </div>
   );
