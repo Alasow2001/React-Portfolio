5ffcc8988f853015616c54f69e4ee8a2564f6f2a
Ahmed Alasow <a.alasow01@gmail.com>
2023-03-01 13:55:34 +0000

    Edited portfolio page

--- react-portfolio/src/components/pages/Portfolio.js
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -3,15 +3,14 @@ import React from 'react';
 export default function Portfolio() {
   return (
     <div>
-      <h1>My Portfolio</h1>
+      <h1>Recent Projects</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        Welcome to my Portfolio page. Here, you can see a list of some of my most recent projects.
       </p>
+
+      <div>
+        <projectList />
+      </div>
     </div>
   );
 }
