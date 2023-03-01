c8c1604b117310e75b78583115be550f9c5ba01e
Ahmed Alasow <a.alasow01@gmail.com>
2023-03-01 20:00:52 +0000

    Adding classes for styling purposes

--- react-portfolio/src/components/pages/Home.js
+++ react-portfolio/src/components/pages/Home.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div>
+    <div className='homepage'>
       <h1>Home Page</h1>
-      <p>
+      <p className='intro'>
         Hello, my name is Ahmed Alasow and I'm currently studying a Web Development bootcamp at the University of Birmingham. 
         Throughout this bootcamp, I have learnt various programming languages, dependencies as well as a few databases. 
         As the bootcamp has progressed, I have gained a significant amount of experience, enjoyment and fascination with the following tools below:
