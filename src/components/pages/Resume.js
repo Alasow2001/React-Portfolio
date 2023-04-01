03ef97bbd0f1d45eb2925903ae3344c9128b56f6
Ahmed Alasow <a.alasow01@gmail.com>
2023-04-01 13:09:50 +0100

    Changed link to CV

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -5,7 +5,7 @@ export default function Resume() {
     <div>
       <h1 className='resume-title'>My Resume</h1>
       <div className='resume-container'>
-        <iframe src='https://docs.google.com/file/d/1B6TJyS21SYOBN5_BDjz7pJoVK5BUUmSTXEuKK2x36SM/preview' title='my-cv' height="900" width="800"></iframe>
+        <iframe src='https://docs.google.com/document/d/16cFaqrxrr7CvJyqSxrdj2AJHxNfzy7Gb/preview' title='my-cv' height="900" width="800"></iframe>
       </div>
     </div>
   );
