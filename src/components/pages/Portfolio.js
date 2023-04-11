c1e5927d9098935727e49baa058da1699dcf6fee
Ahmed Alasow <a.alasow01@gmail.com>
2023-04-11 18:35:47 +0100

    Added responsive elements to the page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,8 +4,8 @@ import ProjectList from '../ProjectList';
 export default function Portfolio() {
   return (
     <div>
-      <h1>Recent Projects</h1>
-      <p>
+      <h1 className='portfolio-title'>Recent Projects</h1>
+      <p className='description'>
         Welcome to my Portfolio page. Here, you can see a list of some of my most recent projects.
       </p>
 
